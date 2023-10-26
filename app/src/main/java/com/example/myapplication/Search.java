9b29c80b1f1b5dbd95c022e9979220d9d173a960
nishorgonil <nishorgonil@gmail.com>
2023-10-27 02:39:39 +0600

    abcdgygugugu

--- app/src/main/java/com/example/myapplication/Search.java
+++ app/src/main/java/com/example/myapplication/Search.java
@@ -3,24 +3,113 @@ package com.example.myapplication;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class Search extends Fragment {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.YouTubeRequestInitializer;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Search extends Fragment implements View.OnClickListener  {
+
+    HttpTransport HTTP_TRANSPORT = new com.google.api.client.http.javanet.NetHttpTransport();
+    JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+
+    private EditText searchBar;
+    private Button searchButton;
+    private RecyclerView videoList;
+    private YouTube youtube;
+    private List<SearchResult> searchResults;
+    private SearchListAdapter searchResultsAdapter;
+    private final String API_KEY = "AIzaSyANCeqsxRm4V1UVDHOl1jQ8VXgJnnsthe4";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         View view= inflater.inflate(R.layout.fragment_search, container, false);
+
+        searchBar = view.findViewById(R.id.searchBar);
+        searchButton = view.findViewById(R.id.searchButton);
+        videoList = view.findViewById(R.id.videoList);
+        videoList.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
+        searchResults = new ArrayList<>();
+        searchResultsAdapter = new SearchListAdapter(searchResults);
+        videoList = view.findViewById(R.id.videoList);
+        videoList.setLayoutManager(new LinearLayoutManager(getActivity()));
+        videoList.setAdapter(searchResultsAdapter);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("SearchButton", "Search button clicked!!!!");
+                searchResults.clear();
+                String query = searchBar.getText().toString();
+                if (!query.isEmpty()) {
+                    searchYoutubeVideos(query);
+                    Integer val = searchResults.size();
+                    Log.d("prob", val.toString());
+                }
+            }
+        });
 
         return view;
     }
 
+
+//
+    private void searchYoutubeVideos(final String query) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    YouTube youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, null)
+                            .setApplicationName("MusicMaster")
+                            .setYouTubeRequestInitializer(new YouTubeRequestInitializer(API_KEY))
+                            .build();
+
+                    searchResults.clear();
+                    YouTube.Search.List search = youtube.search().list("id,snippet");
+                    search.setKey(API_KEY);
+                    search.setQ(query);
+                    search.setType("video");
+                    search.setMaxResults(10L);
+                    SearchListResponse searchResponse = search.execute();
+                    searchResults.addAll(searchResponse.getItems());
+
+                    requireActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            searchResultsAdapter.notifyDataSetChanged();
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace(); // Handle the exception gracefully
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
 }
